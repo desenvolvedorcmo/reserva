58a7239949211f14e125eae6769e06e8cd012a7f
eaedanilo <eaedanilo1@gmail.com>
2021-10-28 10:26:54 -0300

    mural removido

--- mural/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "123";
-	$dbname = "reserva";
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-?>
